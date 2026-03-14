35c96f25795582cafd59d25c7771b5f39e764096
Wix Remote Machine <support@wix.com>
2026-03-14 05:20:33 +0000

    Set 20px gap between cards

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -626,7 +626,7 @@ export default function HomePage() {
                 ))}
               </div>
             ) : processExamples.length > 0 ? (
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2.5">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
                 {processExamples.slice(0, 9).map((process, index) => (
                   <GridProcessCard 
                     key={process._id} 
