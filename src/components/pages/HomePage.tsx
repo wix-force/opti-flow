7caf09bbcfffb9184c8f3d4624199e00d91201e9
Wix Remote Machine <support@wix.com>
2026-03-15 06:33:23 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -450,7 +450,7 @@ export default function HomePage() {
             >
               <div className="mb-8">
                 <h2 className="font-heading text-4xl md:text-5xl lg:text-6xl mb-6 text-primary font-bold leading-tight">THE COST OF INACTION</h2>
-                <p className="font-heading text-xs uppercase tracking-widest text-black font-bold mb-4">Adjust your hourly $ rate and hours spent on messy workflows to see your potential savings.</p>
+                <p className="font-heading text-xl  tracking-widest text-black font-bold mb-4">Adjust your hourly $ rate and hours spent on messy workflows to see your potential savings.</p>
 
               </div>
             </motion.div>
