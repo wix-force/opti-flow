72640fb1862e91519faef4a94b4f934278598a82
Wix Remote Machine <support@wix.com>
2026-03-15 06:38:23 +0000

    Manual code changes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -874,7 +874,7 @@ function GridProcessCard({
           <div className="w-full">
             <p className="font-paragraph text-xs leading-normal">
               <span className="font-heading text-xs font-bold text-primary uppercase tracking-wider block mb-1">
-                Impact:{process.potentialImpact}
+                Impact: 
               </span>
 
             </p>
